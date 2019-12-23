fe831338effb190ff0f5a5552f724bc14b731d43
Konmoron <cqlq2012@gmail.com>
2019-12-23 20:40:50 +0800

    完善 map

--- 13_map/map.go
+++ 13_map/map.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	personSalary := map[string]int {}
+	personSalary := map[string]int{}
 	fmt.Println("map is", personSalary)
 	personSalary["mike"] = 9000
 	fmt.Println("map is", personSalary)
 	for key, value := range personSalary {
 		fmt.Println(key, "=", value)
+	}
+
+	testMap := map[string]string{"A": "A", "B": "B", "C": "C"}
+	for k, v := range testMap {
+		fmt.Println("key:", k, ", value", v)
 	}
 }
